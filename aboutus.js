cfc48beac5b9e0a737315fb9c5e7c54372821c16
EddsCrystal <144728591+EddsCrystal@users.noreply.github.com>
2024-02-03 20:52:52 +0530

    Add files via upload

---
+++ aboutus.js
@@ -0,0 +1,24 @@
+/*********************************** Navbar ****************************************/
+/* Opens side bar */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+    close_side_social_Nav()
+  }
+  
+  /* Closes side bar */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  /*********************************** Navbar ****************************************/
+  
+  /****************************** Social side navbar ********************************/
+  /* close social side navbar */
+  function open_side_social_Nav() {
+    document.getElementById("social_sidenav").style.width = "100px";
+  }
+  
+  /* close social side navbar */
+  function close_side_social_Nav() {
+    document.getElementById("social_sidenav").style.width = "0";
+  }
+  /****************************** Social side navbar ********************************/
---
+++ gallery.js
@@ -0,0 +1,35 @@
+/*********************************** Navbar ****************************************/
+/* Opens side bar */
+function openNav() 
+{
+  document.getElementById("myNav").style.width = "100%";
+  close_side_social_Nav()
+}
+
+/* Closes side bar */
+function closeNav() 
+{
+  document.getElementById("myNav").style.width = "0%";
+}
+/*********************************** Navbar ****************************************/
+
+/****************************** Social side navbar ********************************/
+/* close social side navbar */
+function open_side_social_Nav() 
+{
+  document.getElementById("social_sidenav").style.width = "100px";
+}
+
+/* close social side navbar */
+function close_side_social_Nav() 
+{
+  document.getElementById("social_sidenav").style.width = "0";
+}
+/****************************** Social side navbar ********************************/
+
+/****************************** Scrool to top ********************************/
+function scroll_to_top()
+{
+  window.scrollTo(0, 0);
+}
+/****************************** Scrool to top ********************************/
